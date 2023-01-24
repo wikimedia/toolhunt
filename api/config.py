d4f4feb028c6f09b84cfb8d87e4e7b60992cd320
nbarnabee <nicole.barnabee@gmail.com>
2023-01-24 14:18:32 +0100

    Correct database URI string

--- api/config.py
+++ api/config.py
@@ -5,7 +5,7 @@ class BaseConfig:
   """Base config"""
   BASE_DIR = Path(__file__).parent.parent
   TESTING = False
-  SQLALCHEMY_DATABASE_URI = "mysql://root:test_pass@localhost:3033/test"
+  SQLALCHEMY_DATABASE_URI = "mysql://root:test_pass@db:3306/test"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
