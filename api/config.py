0477effeb7783a48b2c4d178ce9b212fe6e204ed
nbarnabee <nicole.barnabee@gmail.com>
2023-01-24 22:13:27 +0100

    Add Open API config options

--- api/config.py
+++ api/config.py
@@ -7,6 +7,13 @@ class BaseConfig:
   TESTING = False
   SQLALCHEMY_DATABASE_URI = "mysql://username:12345@db:3306/test"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  API_TITLE = "Toolhunt REST API"
+  API_VERSION = "v1"
+  OPENAPI_VERSION = "3.0.3"
+  OPENAPI_URL_PREFIX = "/"
+  OPENAPI_SWAGGER_UI_PATH = "/api/documentation"
+  OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  PROPAGATE_EXCEPTIONS = True
 
 class DevelopmentConfig(BaseConfig):
   """Development config"""
