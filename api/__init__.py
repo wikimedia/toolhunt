c14985adae925973d9600deee14c08fc0e6e6f6f
Nicole Barnabee <95652414+nbarnabee@users.noreply.github.com>
2023-01-25 13:29:15 +0100

    Make the db stuff work (#10)
    Co-authored-by: Slavina Stefanova <sstefanova@wikimedia.org>

--- api/__init__.py
+++ api/__init__.py
@@ -16,8 +16,19 @@ def create_app(config_name=None):
   app = Flask(__name__)
 
   app.config.from_object(config[config_name])
+
   db.init_app(app)
   # migrate.init_app(app, db)
 
 
   return app
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
