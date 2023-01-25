f6465964875ab6d139b1b2da6dd4d54406bb23e3
nbarnabee <nicole.barnabee@gmail.com>
2023-01-25 14:53:17 +0100

    Remove extraneous code and update README

--- api/__init__.py
+++ api/__init__.py
@@ -3,11 +3,11 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 from api.config import config
 
 db = SQLAlchemy()
 migrate = Migrate()
+
 
 def create_app(config_name=None):
   if config_name is None:
@@ -20,15 +20,4 @@ def create_app(config_name=None):
   db.init_app(app)
   # migrate.init_app(app, db)
 
-
   return app
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
