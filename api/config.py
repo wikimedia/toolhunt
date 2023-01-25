96a37de888a9497ab1f69411fcfbf7b952b1adc6
Slavina Stefanova <sstefanova@wikimedia.org>
2023-01-25 11:13:23 +0100

    Make the db stuff work

--- api/config.py
+++ api/config.py
@@ -1,11 +1,11 @@
 import os
-from pathlib import Path 
+from pathlib import Path
 
 class BaseConfig:
   """Base config"""
   BASE_DIR = Path(__file__).parent.parent
   TESTING = False
-  SQLALCHEMY_DATABASE_URI = "mysql://root:test_pass@db:3306/test"
+  SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask.cli import FlaskGroup
+
+from api import db
+from app import app
+
+
+cli = FlaskGroup(app)
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+if __name__ == "__main__":
+    cli()
