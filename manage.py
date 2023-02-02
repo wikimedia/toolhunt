931f472dfea5b0a678cbdca86a3d1dc2213f4b99
nbarnabee <nicole.barnabee@gmail.com>
2023-02-02 07:01:06 +0100

    Add db migration via flask-migrate & alembic

--- manage.py
+++ manage.py
@@ -9,12 +9,6 @@ from data import tool_data, field_data, task_data
 
 cli = FlaskGroup(app)
 
-@cli.command("create_db")
-def create_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
 @cli.command("insert_data")
 def insert_data():
     db.session.bulk_insert_mappings(Tool, tool_data)
