2bbd8b30fb704b4b8697e2c6266bfb26f9d7d615
nbarnabee <nicole.barnabee@gmail.com>
2023-02-11 18:15:24 +0100

    Add CLI command to call insertion functions

--- manage.py
+++ manage.py
@@ -3,18 +3,30 @@ from flask.cli import FlaskGroup
 from api import db
 from app import app
 
-from api.models import Tool, Field, Task
-from data import tool_data, field_data, task_data
+from api.models import Tool, Task, Field
+from api.tests.fixtures.data import tool_data, task_data, field_data
+from api.jobs import populate_db
 
 
 cli = FlaskGroup(app)
 
-@cli.command("insert_data")
-def insert_data():
+@cli.command("insert_mock_data")
+def insert_mock_data():
+    """ Inserts the mock tasks and tools """
     db.session.bulk_insert_mappings(Tool, tool_data)
-    db.session.bulk_insert_mappings(Field, field_data)
     db.session.bulk_insert_mappings(Task, task_data)
     db.session.commit()
+
+@cli.command("insert_fields")
+def run_field_insert():
+    """ Inserts field data """
+    db.session.bulk_insert_mappings(Field, field_data)
+    db.session.commit()
+
+@cli.command("populate_db")
+def run_populate_db():
+    """ Fetches and inserts tool data from Toolhub """
+    populate_db()
 
 
 if __name__ == "__main__":
