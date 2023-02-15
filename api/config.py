0c8efca23b293e8709a762af4f6b879ce5149e44
nbarnabee <nicole.barnabee@gmail.com>
2023-02-15 15:32:32 +0100

    Incorporate code and test page from oauth demo
    I also added a basic test route that will be removed by changes in the
    "build put route" branch.

--- api/config.py
+++ api/config.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class BaseConfig:
   """Base config"""
@@ -14,6 +17,29 @@ class BaseConfig:
   OPENAPI_SWAGGER_UI_PATH = "/api/documentation"
   OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
   PROPAGATE_EXCEPTIONS = True
+
+  # OAuth demo stuff
+  SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    default=os.urandom(128).decode("utf8", errors="ignore"),
+)
+  SERVER_NAME = os.getenv("SERVER_NAME", default="localhost:5000")
+  TOOLHUB_CLIENT_ID = os.getenv("TOOLHUB_CLIENT_ID")
+  TOOLHUB_CLIENT_SECRET = os.getenv("TOOLHUB_CLIENT_SECRET")
+  TOOLHUB_ACCESS_TOKEN_URL = os.getenv(
+    "TOOLHUB_ACCESS_TOKEN_URL",
+    default="http://web:5000/o/token/",
+)
+  TOOLHUB_AUTHORIZE_URL = os.getenv(
+    "TOOLHUB_AUTHORIZE_URL",
+    default="https://localhost:5000/authorize/",
+)
+  TOOLHUB_API_BASE_URL = os.getenv(
+    "TOOLHUB_API_BASE_URL",
+    default="http://web:5000/api/",
+)
+
+
 
 class DevelopmentConfig(BaseConfig):
   """Development config"""
