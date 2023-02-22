9f5d2cc8378f85fe9222bcb274351192ad6e540d
Nicole Barnabee <95652414+nbarnabee@users.noreply.github.com>
2023-02-22 16:19:33 +0100

    Refactor api and oauth initialization (#35)
    This references T330263

--- api/__init__.py
+++ api/__init__.py
@@ -3,8 +3,12 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_smorest import Api 
 from flask_sqlalchemy import SQLAlchemy
+from authlib.integrations.flask_client import OAuth
+
+api = Api()
 db = SQLAlchemy()
 migrate = Migrate()
+oauth = OAuth()
 
 from api.routes import fields, contributions, tasks, user
 from api.config import config
@@ -16,13 +20,14 @@ def create_app(config_name=None):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
 
-  api = Api(app)
+  api.init_app(app)
   db.init_app(app)
+  oauth.init_app(app)
+  oauth.register(name="toolhub")
   api.register_blueprint(tasks)
   api.register_blueprint(contributions)
   api.register_blueprint(fields)
   api.register_blueprint(user)
   migrate.init_app(app, db)
-
 
   return app
