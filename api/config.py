ce35f120d85e09d208596b94d9a682a0fff9d46a
Nicole Barnabee <95652414+nbarnabee@users.noreply.github.com>
2023-02-22 16:19:52 +0100

    Switch Swagger-ui asset origin to Toolforge CDNjs (#36)
    We don't want to make Toolforge unhappy.
    This addresses T330260

--- api/config.py
+++ api/config.py
@@ -15,7 +15,7 @@ class BaseConfig:
   OPENAPI_VERSION = "3.0.3"
   OPENAPI_URL_PREFIX = "/"
   OPENAPI_SWAGGER_UI_PATH = "/api/documentation"
-  OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  OPENAPI_SWAGGER_UI_URL = "https://tools-static.wmflabs.org/cdnjs/ajax/libs/swagger-ui/4.15.5/"
   PROPAGATE_EXCEPTIONS = True
 
   # OAuth
