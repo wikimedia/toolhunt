fb348e750ccb857f011f6f5bb262406dbe634c98
nbarnabee <nicole.barnabee@gmail.com>
2023-03-07 15:04:33 +0100

    Declare global BASE_DIR and add Path
    This commit addresses changes requested by Slavina.

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 from flask.cli import FlaskGroup
 
@@ -8,13 +9,13 @@ from app import app
 from jobs.populate_db import insert_into_db, run_bulk_population_job
 
 cli = FlaskGroup(app)
+BASE_DIR = app.config["BASE_DIR"]
 
 
 @cli.command("insert_fields")
 def insert_fields():
     """Insert data about annotations fields into the DB"""
-    BASE_DIR = app.config["BASE_DIR"]
-    with open(f"{BASE_DIR}/tests/fixtures/fields.json") as fields:
+    with open(Path(f"{BASE_DIR}/tests/fixtures/fields.json")) as fields:
         field_data = json.load(fields)
         db.session.bulk_insert_mappings(Field, field_data)
         db.session.commit()
@@ -29,8 +30,7 @@ def run_population_job():
 @cli.command("run_test_population")
 def run_populate_db_test():
     """Inserts the test tool and task data into db"""
-    BASE_DIR = app.config["BASE_DIR"]
-    with open(f"{BASE_DIR}/tests/fixtures/data.json") as data:
+    with open(Path(f"{BASE_DIR}/tests/fixtures/data.json")) as data:
         test_data = json.load(data)
         test_tool_data = test_data[0]["tool_data"]
         insert_into_db(test_tool_data)
