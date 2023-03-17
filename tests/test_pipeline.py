5f92b2e57a7c6f68b8e4f5742cb2260cb775b3b8
nbarnabee <nicole.barnabee@gmail.com>
2023-03-17 19:35:14 +0100

    Add basic logging to test pipeline

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,12 +1,28 @@
 import datetime
+import logging
 
 from jobs.populate_db import clean_tool_data, update_task_table, update_tool_table
 
+logging.basicConfig(
+    filename="test_pipeline.log",
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    filemode="w",
+    encoding="utf-8",
+    level=logging.INFO,
+)
 
-def run_test_pipeline(raw_data_set) -> None:
+logger = logging.getLogger()
+
+
+def run_test_pipeline(raw_data_set):
     # Transform
+    logger.info("Cleaning tool data")
     tools_clean_data = clean_tool_data(raw_data_set)
+    logger.info(f"Results: {tools_clean_data}")
+    logger.info("Moving to tool update")
     # Load
     timestamp = datetime.datetime.now(datetime.timezone.utc)
     update_tool_table(tools_clean_data, timestamp)
-    update_task_table(tools_clean_data)
+    logger.info("Tools updated")
+    logger.info("Updating tasks")
+    update_task_table(tools_clean_data, timestamp)
