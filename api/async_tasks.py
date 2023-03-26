e5225b98913001cfb2cef6a14d94c72bd8e2bb31
nbarnabee <nicole.barnabee@gmail.com>
2023-03-26 13:26:35 +0200

    Improve results and error handling
    Removed extraneous print() messages
    Clarified results so that the workflow is clearer
    Attempted to catch additional exceptions on db update failure

--- api/async_tasks.py
+++ api/async_tasks.py
@@ -19,8 +19,7 @@ def make_put_request(self, tool_name, submission_data, token):
         print(f"Exception {e} raised, retrying after 5 seconds...")
         raise self.retry(exc=e, countdown=5)
     else:
-        print(f"Put request was successful! Task result: {result}")
-        return result
+        return f"PUT request made.  Response from Toolhub: {result}"
 
 
 @shared_task(bind=True, name="toolhunt-api.tasks.check_result_status")
@@ -30,20 +29,18 @@ def check_result_status(self, result, edited_field, submitted_value):
     if "code" in result:
         if result["code"] == 4004:
             message = "404 error received; No such tool found in Toolhub's records."
-            print(message)
             return message
         elif result["code"] == "1000":
             res_message = result["errors"][0]["message"]
             message = f"Validation failure. Submitted {submitted_value} for {edited_field}, received following error: {res_message}"
-            print(message)
             return message
     else:
-        return "Status check passed"
+        return "Status check passed; Toolhub records updated successfully."
 
 
 @shared_task(bind=True, name="toolhunt-api.tasks.update_db")
 def update_db(self, result, task_id, form_data, tool_title):
-    if result == "Status check passed":
+    if result == "Status check passed; Toolhub records updated successfully.":
         task = Task.query.get(task_id)
         completedTask = CompletedTask(
             tool_name=form_data["tool"],
@@ -56,11 +53,9 @@ def update_db(self, result, task_id, form_data, tool_title):
             db.session.add(completedTask)
             db.session.delete(task)
             db.session.commit()
-            print("DB update successful.")
-            return
-        except exc.DBAPIError as err:
+            return "Toolhunt database successfully updated."
+        except (exc.DBAPIError, exc.SQLAlchemyError) as err:
             print(err)
-            raise self.retry(exc=err, countdown=5)
+            raise self.retry(exc=err, countdown=10)
     else:
-        print(result)
-        return
+        return f"Submission failure. Reason: {result}"
